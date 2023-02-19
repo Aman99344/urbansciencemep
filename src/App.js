c856565acfb1bfe655eb6beabd558a597dd83ac9
Aman Gupta <amangupta99344@gmail.com>
2023-02-19 21:19:25 +0530

    urbansciencemep weekly test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/HomePage/header';
+import Projects from './Components/HomePage/Projects';
+import Footer from './Components/HomePage/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
